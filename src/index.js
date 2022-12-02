c8b51ded281c2c3c48caa76323f189534c878e9b
Eric Ramirez <ramirez1990rer@gmail.com>
2022-12-02 03:45:18 +0200

    added url

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 
-console.log(axios.isCancel('something'));
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '31766486-572375a92de9bb4d66deb6c09';
+
+export default async function getImages() {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=dog&page=1&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
+    );
+    console.log(response.data.hits);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getImages();
