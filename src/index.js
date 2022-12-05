6908b5c20a51148a19b36307e0167be44cc09a52
Eric Ramirez <ramirez1990rer@gmail.com>
2022-12-05 13:45:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const refs = {
   guard: document.querySelector('.js-guard'),
 };
 
-let page = 0;
+let page = 1;
+let searchQuery = '';
+let totalImages = 0;
 
 const options = {
   root: null,
@@ -18,20 +20,6 @@ const options = {
 
 const observer = new IntersectionObserver(onLoad, options);
 
-function onLoad(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      page += 1;
-      getImages().then(({ data }) => {
-        refs.divGallery.insertAdjacentHTML(
-          'beforeend',
-          createMarkup(data.hits)
-        );
-      });
-    }
-  });
-}
-
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '31766486-572375a92de9bb4d66deb6c09';
 
@@ -40,31 +28,39 @@ refs.form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  const searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
+  searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
 
   if (!searchQuery) {
     clearGalleryList();
     return;
   }
 
-  getImages(searchQuery).then(({ data }) => {
+  getImages(searchQuery, page).then(({ data }) => {
     if (!data.totalHits) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+      clearGalleryList();
       return;
     }
-    // clearGalleryList();
+
+    totalImages += data.hits.length;
+    if (totalImages > data.totalHits) {
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      return;
+    }
     Notify.success(`Hooray! We found ${data.totalHits} images.`);
     refs.divGallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
     observer.observe(refs.guard);
   });
 }
 
-async function getImages(inputValue, page = 1) {
+async function getImages(searchQuery, page = 1) {
   try {
     const response = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${inputValue}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
+      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&page=${page}&per_page=200&image_type=photo&orientation=horizontal&safesearch=true`
     );
     const { data } = response;
 
@@ -104,6 +100,20 @@ function createMarkup(arr) {
 </div>`
     )
     .join('');
+}
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      getImages(searchQuery, page).then(({ data }) => {
+        refs.divGallery.insertAdjacentHTML(
+          'beforeend',
+          createMarkup(data.hits)
+        );
+      });
+    }
+  });
 }
 
 function clearGalleryList() {
